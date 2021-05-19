3e18c79343345108f0a738df5a9b910575690234
Anita O <anita.orstad@gmail.com>
2021-05-19 12:14:49 +0200

    adding homepage spring boot

---
+++ homepage/src/test/java/no/betaniaknarvik/homepage/HomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package no.betaniaknarvik.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
